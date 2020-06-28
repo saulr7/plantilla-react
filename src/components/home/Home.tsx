b33b0238c8b37af10de5f2adad1a4ce85a79c83e
Sau <fernando.ramire@hotmail.com>
2020-06-27 20:12:32 -0600

    initial commit

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+
+const Home : React.FC<{}> = ()=> {
+
+
+    return (
+
+        <div>
+
+            <div className="container">
+
+                <div className="row mt-3">
+                    <div className="col" >
+                        <h3 className="font-weight-bold">Home</h3>
+                    </div>
+                </div>
+
+                <div className="row mt-2">
+                    <div className="col">
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, assumenda laborum. Expedita optio eaque ipsa vel, tempora quaerat quis maiores quasi beatae nostrum, veniam modi labore temporibus sed. Nam, nulla?
+                        </p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="card-columns">
+                            <div className="card my-card">
+                                <div className="card-body">
+                                    <div className="text-center">
+                                        <i className="fas fa-ambulance fa-2x text-success"></i>
+
+                                    </div>
+                                    <br/>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis magnam ipsum dolor, cupiditate omnis cumque alias nulla, laudantium enim culpa eos molestias facere quia, rem libero dicta reprehenderit magni similique!
+                                </div>
+                            </div>
+                            <div className="card my-card p-3">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur animi maxime rerum asperiores eius iure voluptate sit dignissimos, soluta ipsam repellat molestiae voluptatem ipsum quam porro, omnis, blanditiis sequi maiores.
+                            </div>
+                            <div className="card my-card">
+                                <div className="card-body">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis magnam ipsum dolor, cupiditate omnis cumque alias nulla, laudantium enim culpa eos molestias facere quia, rem libero dicta reprehenderit magni similique!
+                                </div>
+                            </div>
+                            <div className="card my-card p-3">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur animi maxime rerum asperiores eius iure voluptate sit dignissimos, soluta ipsam repellat molestiae voluptatem ipsum quam porro, omnis, blanditiis sequi maiores.
+                            </div>
+                            <div className="card my-card p-3">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur animi maxime rerum asperiores eius iure voluptate sit dignissimos, soluta ipsam repellat molestiae voluptatem ipsum quam porro, omnis, blanditiis sequi maiores.
+                            </div>
+                            <div className="card my-card">
+                                <div className="card-body">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit illo quidem nostrum ex eius quis hic, sunt dolorem, exercitationem provident neque? Corporis ea quis deserunt dignissimos nostrum nobis et exercitationem?
+                                </div>
+                            </div>
+                            <div className="card my-card bg-primary p-3 text-center text-white">
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure officia ex fugiat possimus, inventore, dignissimos ad molestiae recusandae est qui itaque atque aliquam dolores laboriosam libero soluta natus velit repudiandae!
+                            </div>
+                            <div className="card my-card p-3">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur animi maxime rerum asperiores eius iure voluptate sit dignissimos, soluta ipsam repellat molestiae voluptatem ipsum quam porro, omnis, blanditiis sequi maiores.
+                            </div>
+                            <div className="card my-card">
+                                <div className="card-body">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit illo quidem nostrum ex eius quis hic, sunt dolorem, exercitationem provident neque? Corporis ea quis deserunt dignissimos nostrum nobis et exercitationem?
+                                </div>
+                            </div>
+                            <div className="card my-card bg-primary p-3 text-center text-white">
+                                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure officia ex fugiat possimus, inventore, dignissimos ad molestiae recusandae est qui itaque atque aliquam dolores laboriosam libero soluta natus velit repudiandae!
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
